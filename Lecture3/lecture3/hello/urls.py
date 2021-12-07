ab1cd908f1510ea6985e3f4f018d3cf2ec45b5bf
aimanyounises1 <aymanyounis@icloud.com>
2021-12-07 14:16:16 +0200

    django

--- Lecture3/lecture3/hello/urls.py
+++ Lecture3/lecture3/hello/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path("" , views.index, name= "index")
+    path("" , views.index, name= "index"),
+    path("aiman",views.aiman, name="aiman"),
+    path("<str:name>" , views.greet ,name= "greet") # will pass the name as a parameter
 ]
--- Lecture3/lecture3/hello/views.py
+++ Lecture3/lecture3/hello/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello , world")
+
+def aiman(request):
+    return HttpResponse("Hello , Aiman")
+
+
+def greet(request , name):
+    return HttpResponse(f"Hello, {name}!")
