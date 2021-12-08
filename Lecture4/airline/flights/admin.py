535d3a4a51b805814378973a4c70b33bbc2393d3
aimanyounises1 <aymanyounis@icloud.com>
2021-12-09 01:57:18 +0200

    added passengers to flights

--- Lecture4/airline/flights/admin.py
+++ Lecture4/airline/flights/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Flight , Airport
+# Register your models here. 
+# It will tell django that we would to be able to manipulate airport and flights 
+admin.site.register(Airport)
+admin.site.register(Flight)
--- Lecture4/airline/flights/models.py
+++ Lecture4/airline/flights/models.py
@@ -1,10 +1,38 @@
 from django.db import models
 
 # Create your models here. every model is python class
+
+'''
+origin = models.ForeignKey(Airport , on_delete = models.CASCADE, related_name = "departures")
+    destination = models.ForeignKey(Airport , on_delete= models.CASCADE, related_name = "arrivals")
+    duration = models.IntegerField()'''
+
+
+class Airport(models.Model):
+    code = models.CharField(max_length = 3)
+    city = models.CharField(max_length = 64)
+
+    def __str__(self):
+        return f"{self.code} ({self.city})"
+
+
 class Flight(models.Model):
-    origin = models.CharField(max_length= 64)
-    destination = models.CharField(max_length= 64)
+    origin = models.ForeignKey(Airport , on_delete = models.CASCADE , related_name ="departures") 
+    destination = models.ForeignKey(Airport , on_delete = models.CASCADE , related_name ="arrivals")
     duration = models.IntegerField()
 
     def __str__(self):
-        return f"{self.id}:{self.origin} to {self.destination} time {self.duration}"
+        return f"{self.origin} to {self.destination} time {self.duration}"
+
+
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length =64)
+    # blank is equal to true to allow that the passenger aren't registered to any flights.
+    # ManytoMany relationships means that passenger can have more than one flight
+    # passenger is to access all the passengers on that plane
+    flights = models.ManyToManyField(Flight , blank=True ,related_name ="passengers")
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
