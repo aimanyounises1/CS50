28fa1a7627c19dfd6984919287b7fa771aa4fd44
aimanyounises1 <aymanyounis@icloud.com>
2021-12-08 16:39:32 +0200

    started sqlite3

---
+++ Lecture4/inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO flights (origin , destination , duration) VALUES ("TEL AVIV" , "NEW YORK" ,16);
+INSERT INTO flights (origin , destination , duration) VALUES ("ISTANBUL" , "PARIS" ,8);
+INSERT INTO flights (origin , destination , duration) VALUES ("ROME" , "TEL AVIV" ,5);
+INSERT INTO flights (origin , destination , duration) VALUES ("TEL AVIV" ,"DUBAI" , 7);
+INSERT INTO flights (origin , destination , duration) VALUES ("LAS VEGAS" , "SUADI ARABIA" ,5);
